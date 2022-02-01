b7c0d772893b7451b9eb787afddcefe1ffd6bafb
rav8657 <rav8657@gmail.com>
2022-02-01 11:03:17 +0530

    finalCode

--- src/validators/validator.js
+++ src/validators/validator.js
@@ -1,54 +1,27 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
+// Validation checking function
 const isValid = function (value) {
-    if (typeof value === 'undefined' || value === null) return false
-    if (typeof value === 'string' && value.trim().length === 0) return false
-    return true;
-}
-
-const isValidRequestBody = function (requestBody) {
-    return Object.keys(requestBody).length > 0
-}
-
+  if (typeof value === "undefined" || value === null) return false; //it checks whether the value is null or undefined.
+  if (typeof value === "string" && value.trim().length === 0) return false; //it checks whether the string contain only space or not
+  return true;
+};
 const isValidObjectId = function (objectId) {
-    return mongoose.Types.ObjectId.isValid(objectId)
-}
+  return mongoose.Types.ObjectId.isValid(objectId);
+};
+const isValidRequestBody = function (requestBody) {
+  return Object.keys(requestBody).length > 0; // it checks, is there any key is available or not in request body
+};
 
 //only check empty string value.
 const validString = function (value) {
-    if (typeof value === 'string' && value.trim().length === 0) return false
-    return true;
-}
-//for product
-const validInstallment = function isInteger(value) {
-    if(value < 0) return false
-     if(value % 1 == 0 ) return true
-}
-// const validQuantity = function isInteger(value) {
-//     if(value < 1) return false
-//      if(value % 1 == 0 ) return true
-// }
-
-//for product
-const validQuantity = function isInteger(value) {
-    if (value < 1) return false
-    if (isNaN(Number(value))) return false
-    if (value % 1 == 0) return true
-}
-
-//for order
-const isValidStatus = function(status) {
-    return ['pending', 'completed', 'cancelled'].indexOf(status) !== -1
-}
-
+  if (typeof value === "string" && value.trim().length === 0) return false; //it checks whether the string contain only space or not
+  return true;
+};
 
 module.exports = {
-    isValid,
-    isValidRequestBody,
-    isValidObjectId,
-    validString,
-    validInstallment,
-    validQuantity,
-    isValidStatus
-
-}
+  isValid,
+  isValidRequestBody,
+  isValidObjectId,
+  validString
+};
